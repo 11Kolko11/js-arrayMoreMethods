b4c75fa2f516c71bf610c4f0bd3b566f6255516e
11Kolko11 <kn24-i.trush@nubip.edu.ua>
2025-11-24 23:32:45 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,18 +1,22 @@
-// Функція отримує масив товарів і повертає всі товари, які мають ціну нижчу за 100 або вищу за 500
-"ВИКОРИСТОВУВАТИ ЛИШЕ МЕТОДИ МАСИВІВ filter, map, sort та інші, які є в файлі methods.js."
+// Функція отримує масив товарів і повертає всі товари,
+// які мають ціну нижчу за 100 або вищу за 500
+// "ВИКОРИСТОВУВАТИ ЛИШЕ МЕТОДИ МАСИВІВ filter, map, sort та інші, які є в файлі methods.js."
 
 const products = [
-  { name: 'Laptop', price: 800 },
-  { name: 'Phone', price: 250 },
-  { name: 'Shirt', price: 30 },
-  { name: 'Watch', price: 120 },
-  { name: 'Tablet', price: 550 },
-  { name: 'Shoes', price: 75 },
+  { name: 'Laptop',      price: 800 },
+  { name: 'Phone',       price: 250 },
+  { name: 'Shirt',       price: 30 },
+  { name: 'Watch',       price: 120 },
+  { name: 'Tablet',      price: 550 },
+  { name: 'Shoes',       price: 75 },
   { name: 'PlayStation', price: 590 }
 ];
 
 function getProductsNotInPriceRange(products) {
-  // Ваш код
+  // залишаємо тільки ті товари, де ціна < 100 або > 500
+  return products.filter(function (product) {
+    return product.price < 100 || product.price > 500;
+  });
 }
 
 console.log(getProductsNotInPriceRange(products));
